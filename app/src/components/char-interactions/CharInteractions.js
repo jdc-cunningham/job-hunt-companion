4cd6ccc5ea8d3b4b983538e38a14e2ab4775d7e4
Jacob Cunningham <jcun4128@gmail.com>
2024-02-17 19:59:55 -0600

    use transparent char

--- app/src/components/char-interactions/CharInteractions.js
+++ app/src/components/char-interactions/CharInteractions.js
@@ -4,7 +4,9 @@ import "./CharInteractions.scss";
 const CharacterInteractions = (props) => {
   return (
     <div className="CharacterInteractions">
-      <img class="CharacterInteractions__character" src={Char} alt="anime character"/>
+      <div class="CharacterInteractions__character-bg">
+        <img class="CharacterInteractions__character" src={Char} alt="anime character"/>
+      </div>
       <h2>Job-chan</h2>
       <textarea className="CharacterInteractions__input" value="Have you applied to any jobs today?"/>
     </div>
